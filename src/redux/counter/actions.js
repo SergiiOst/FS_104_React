cd86d2b6ea0eed6d6551ed4a5461f6a9c77067be
SergiiOst <sergeyostapenko85@gmail.com>
2024-08-06 18:02:49 +0300

    Create Reducer

--- src/redux/counter/actions.js
+++ src/redux/counter/actions.js
@@ -1,23 +1,28 @@
-import { CHANGE_STEP, DECREMENT, INCREMENT, RESET } from "./constants";
+import { createAction } from "@reduxjs/toolkit";
 
-export const increment = () => {
-  return {
-    type: INCREMENT,
-  };
-};
+// export const increment = () => {
+//   return {
+//     type: INCREMENT,
+//   };
+// };
 
-export const decrement = () => {
-  return {
-    type: DECREMENT,
-  };
-};
+// export const decrement = () => {
+//   return {
+//     type: DECREMENT,
+//   };
+// };
 
-export const reset = () => {
-  return {
-    type: RESET,
-  };
-};
+// export const reset = () => {
+//   return {
+//     type: RESET,
+//   };
+// };
 
-export const changeStep = (step) => {
-  return { type: CHANGE_STEP, payload: step };
-};
+// export const changeStep = (step) => {
+//   return { type: CHANGE_STEP, payload: step };
+// };
+
+export const increment = createAction("increment");
+export const decrement = createAction("decrement");
+export const reset = createAction("reset");
+export const changeStep = createAction("change_step");
